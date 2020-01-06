026973f11b0272aa41375ce1d7c65577f359dd60
Shona McKenzie <smckenzie@blueboxaviation.com>
2020-01-06 13:23:41 +0000

    weather-app web server

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const express = require('express')
+
+const app = express()
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+app.use(express.static(publicDirectoryPath))
+
+// remove route handler as using static assets in the form of files in public folder
+// app.get('', (req, res) => {
+//     res.send('Hello express!')
+// })
+
+// app.get('/help', (req, res) => {
+//     res.send('Help page')
+// })
+
+// app.get('/about', (req, res) => {
+//     res.send('<h1>About</h1>')
+// })
+
+app.get('/weather', (req, res) => {
+    res.send({
+        location: "Edinburgh",
+        forecast: "sunshine 30 degrees"
+    })
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+    
+})
