23e591167bd1bb32f67a101b6ba22e00e243a445
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-01-12 17:27:17 +0000

    update weather route to use geocode and forecast

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const express = require('express')
 const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
 
 const app = express()
 
@@ -57,12 +59,28 @@ app.get('/weather', (req, res) => {
     return res.send({
       error: 'You must provide an address'
     })
-  }
-  res.send({
-    location: "Edinburgh",
-    forecast: "sunshine 30 degrees",
-    address: req.query.address
-  })
+  }else (
+    geocode(req.query.address, (error, {latitude, longitude, location}) => {
+      if (error){
+        return res.send({
+          error: 'error'
+        })
+      }else {
+        forecast(latitude, longitude, (error, forecastData) => {
+          if (error){
+            return res.send({
+              error: 'error'
+            })
+        }
+        res.send({
+          location: location,
+          forecast: forecastData,
+          address: req.query.address
+        })
+      })
+    }
+    })
+  )  
 })
 
 app.get('/products', (req, res) => {
