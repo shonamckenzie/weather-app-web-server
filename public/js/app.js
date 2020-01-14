fcaaba95fc4524677d84593967fce986bce36db4
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-01-14 19:44:15 +0000

    add fetch request for location data

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
 console.log('Client side Javascript file is loaded');
+
+  fetch('http://localhost:3000/weather?address=Boston')
+    .then((res) => {
+      res.json()
+        .then((data) => {
+            if (data.error) {
+              console.log(data.error)           
+            }else {
+              console.log(data.location);   
+              console.log(data.forecast);
+              
+            }     
+        })
+    })
