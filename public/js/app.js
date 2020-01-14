3536b6ae036d13176336a823d5792a55d5a8efdc
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-01-14 21:14:52 +0000

    adds user interface to render in browser

--- public/js/app.js
+++ public/js/app.js
@@ -10,17 +10,21 @@ const handleNewWeatherFormSubmit = (event) => {
 
   const search = document.querySelector('input')
   const location = search.value
+  const messageOne = document.querySelector('#message-1')
+  const messageTwo = document.querySelector('#message-2')
 
+  messageOne.textContent = 'Loading...'
+  messageTwo.textContent = ''
+  
   fetch(`http://localhost:3000/weather?address=${location}`)
   .then((res) => {
     res.json()
       .then((data) => {
           if (data.error) {
-            console.log(data.error)           
+            messageOne.textContent = data.error      
           }else {
-            console.log(data.location);   
-            console.log(data.forecast);
-            
+            messageOne.textContent = data.location   
+            messageTwo.textContent = data.forecast        
           }     
       })
   })
