c3a8b8a61e258f118d6e6443d81f81ace8063af3
Shona McKenzie <shona_mckenzie@hotmail.com>
2020-01-14 20:59:41 +0000

    adds search form

--- public/js/app.js
+++ public/js/app.js
@@ -1,15 +1,27 @@
 console.log('Client side Javascript file is loaded');
 
-  fetch('http://localhost:3000/weather?address=Boston')
-    .then((res) => {
-      res.json()
-        .then((data) => {
-            if (data.error) {
-              console.log(data.error)           
-            }else {
-              console.log(data.location);   
-              console.log(data.forecast);
-              
-            }     
-        })
-    })
+document.addEventListener('DOMContentLoaded', () => {
+  const weatherForm = document.querySelector('form')
+  weatherForm.addEventListener('submit', handleNewWeatherFormSubmit);
+}) 
+
+const handleNewWeatherFormSubmit = (event) => {
+  event.preventDefault()
+
+  const search = document.querySelector('input')
+  const location = search.value
+
+  fetch(`http://localhost:3000/weather?address=${location}`)
+  .then((res) => {
+    res.json()
+      .then((data) => {
+          if (data.error) {
+            console.log(data.error)           
+          }else {
+            console.log(data.location);   
+            console.log(data.forecast);
+            
+          }     
+      })
+  })
+}
