ab922a692dae97684f806445e0154c03ef852b13
Shona McKenzie <smckenzie@blueboxaviation.com>
2020-01-15 13:38:10 +0000

    heroku config

--- public/js/app.js
+++ public/js/app.js
@@ -16,7 +16,7 @@ const handleNewWeatherFormSubmit = (event) => {
   messageOne.textContent = 'Loading...'
   messageTwo.textContent = ''
   
-  fetch(`http://localhost:3000/weather?address=${location}`)
+  fetch(`/weather?address=${location}`)
   .then((res) => {
     res.json()
       .then((data) => {
